13316e6b1148f135290cebdf9097b51bc1260eb5
Aliz <alicja_bobko@o2.pl>
2023-10-26 13:49:15 +0200

    loading states, racing data

--- applications/loading-states/script.js
+++ applications/loading-states/script.js
@@ -10,3 +10,27 @@ import {
   form,
   fetchData,
 } from './utilities';
+
+const loading$ = fromEvent(form, 'submit').pipe(
+  exhaustMap(() => {
+    const data$ = fetchData().pipe(shareReplay(1));
+
+    const showLoading$ = of(true).pipe(
+      delay(+showLoadingAfterField.value),
+      tap((value) => showLoading(value)),
+    );
+
+    const timeToHideTheLoading$ = timer(+showLoadingForAtLeastField.value).pipe(
+      first(),
+    );
+
+    const shouldShowLoading$ = concat(
+      showLoading$,
+      timeToHideTheLoading$,
+      data$.pipe(tap(() => showLoading(false))),
+    );
+    return race(data$, shouldShowLoading$);
+  }),
+);
+
+loading$.subscribe();
