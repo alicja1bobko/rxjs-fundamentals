556bcbc558f275eea542f4dc345844b763f3f556
Aliz <alicja_bobko@o2.pl>
2023-10-26 11:16:09 +0200

    fetching from API

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,14 +1,19 @@
 import { fromEvent, interval, merge, NEVER } from 'rxjs';
 import { setCount, startButton, pauseButton } from './utilities';
-import { scan, skipUntil, takeUntil } from 'rxjs/operators';
+import { scan, skipUntil, takeUntil, switchMap, mapTo } from 'rxjs/operators';
 
-const start$ = fromEvent(startButton, 'click');
-const pause$ = fromEvent(pauseButton, 'click');
+const start$ = fromEvent(startButton, 'click').pipe(mapTo(true));
+const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false));
 
-const counter$ = interval(1000).pipe(
-  skipUntil(start$),
+const counter$ = merge(start$, pause$).pipe(
+  switchMap((shouldIBeRunning) => {
+    if (shouldIBeRunning) {
+      return interval(1000);
+    } else {
+      return NEVER;
+    }
+  }),
   scan((total) => total + 1, 0),
-  takeUntil(pause$),
 );
 
 counter$.subscribe(setCount);
--- applications/dog-facts/script.js
+++ applications/dog-facts/script.js
@@ -21,4 +21,42 @@ import {
   setError,
 } from './utilities';
 
-const endpoint = 'http://localhost:3333/api/facts';
+const endpoint =
+  'http://localhost:3333/api/facts?delay=2000&chaos=true&flakiness=2';
+
+const fetchData = () => {
+  return fromFetch(endpoint).pipe(
+    tap(clearError),
+    mergeMap((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong');
+      }
+    }),
+    retry(4),
+    catchError((error) => {
+      console.warn(error);
+      return of({ error: error.message });
+    }),
+  );
+};
+
+const fetch$ = fromEvent(fetchButton, 'click').pipe(mapTo(true));
+const stop$ = fromEvent(stopButton, 'click').pipe(mapTo(false));
+
+const factStream$ = merge(fetch$, stop$).pipe(
+  switchMap((shouldFetch) => {
+    if (shouldFetch) {
+      return timer(0, 5000).pipe(
+        tap(() => clearError()),
+        tap(() => clearFacts()),
+        exhaustMap(fetchData),
+      );
+    } else {
+      return NEVER;
+    }
+  }),
+);
+
+fetch$.subscribe(addFacts);
